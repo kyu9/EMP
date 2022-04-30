c3ee49bfeff1b6db85c6645aea6887e7451fd26c
kyu9 <kyudo97@naver.com>
2022-04-30 15:05:16 +0900

    [EMP] swagger-codegen 잘못 만든 파일들 삭제

--- src/main/java/com/example/emp/View/controller/ViewController.java
+++ src/main/java/com/example/emp/View/controller/ViewController.java
@@ -5,11 +5,4 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class ViewController {
 
-    @GetMapping("/")
-    public String testController(Model model){
-        return "test";
-    }
-}
